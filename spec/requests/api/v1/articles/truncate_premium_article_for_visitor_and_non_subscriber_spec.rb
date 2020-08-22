d81d29d98fc997aa06a48c1262e275fa773b8fcc
Sima <sima.boreiri@gmail.com>
2020-08-22 14:48:39 +0200

    adds test for non subscriber that shouldn't see full content

--- spec/requests/api/v1/articles/truncate_premium_article_for_visitor_and_non_subscriber_spec.rb
+++ spec/requests/api/v1/articles/truncate_premium_article_for_visitor_and_non_subscriber_spec.rb
@@ -19,4 +19,22 @@ RSpec.describe "GET /v1/articles for premuim", type: :request do
       expect(response_json["article"]["content"].length).to eq 1650
     end
   end
+
+  describe 'non subscriber should not see the full content' do
+    let!(:user){create(:user, role: 'registered')}
+    let(:user_credentials) { user.create_new_auth_token }
+    let(:user_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
+
+    before do
+      get "/api/v1/articles/#{article.id}", headers: user_headers
+    end
+
+    it 'responds with 200 status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'should respond with the 50 charecters of the content' do
+      expect(response_json["article"]["content"].length).to eq 50
+    end
+  end
 end 
